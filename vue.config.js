0a59f89ae6d4cb05d0ee8cd3f61b245142ab3b6a
ljp_git <1825107298@qq.com>
2021-04-29 08:47:07 +0800

    af

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,10 @@ module.exports = {
   configureWebpack:{
     resolve:{
       alias:{
-      
+        'assets':'@/assets',
+        'common':'@/common',
+        'components':'@/components',
+        'network':'@/network'
       }
     }
   }
